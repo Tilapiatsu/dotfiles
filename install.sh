50656b7775e5b7623307132c9120629c2bd5f5fc
Tilapiatsu <tilapiatsu@hotmail.fr>
2025-12-16 22:56:03 +0100

    + add nvim submodule

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-pacman -Syu
-pacman -S yay
-yay -S pacseek 7zip blender brave-bin dolphin freerdp gnome-calculator greetd evtest heroic-games-launcher-bin hwinfo kitty lazygit onlyoffice-bin krita obs-studio okular signal-desktop steam spotify-launcher stow sunshine teams-for-linux vlc vscodium  webapp-manager  xnviewmp yazi 
+#pacman -Syu
+#pacman -S yay
+yay -S pacseek 7zip blender brave-bin dolphin freerdp gnome-calculator greetd evtest heroic-games-launcher-bin hwinfo kitty lazygit onlyoffice-bin krita neovim obs-studio okular signal-desktop steam spotify-launcher stow sunshine teams-for-linux vlc vscodium  webapp-manager  xnviewmp yazi 
 #yay -S xf86-input-libinput xf86-input-wacom wacom-utility-git wacomtablet input-wacom-dkms-git
 #yay -S winboat 

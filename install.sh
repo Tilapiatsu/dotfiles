02ca24e9ea6b9b7968870887e74752765fffc295
Tilapiatsu <tilapiatsu@hotmail.fr>
2025-12-27 18:42:22 +0100

    * Update Dotfiles
    + add packsea as default package installer

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #pacman -Syu
 #pacman -S yay
-yay -S archlinux-xdg-menu pacseek 7zip blender brave-bin dolphin freerdp gnome-calculator greetd evtest heroic-games-launcher-bin hwinfo kitty lazygit onlyoffice-bin krita neovim networkmanager-openvpn networkmanager-vpn-plugin-openconnect networkmanager-vpn-plugin-openvpn obs-studio okular signal-desktop steam spotify-launcher stow sunshine teams-for-linux vlc vscodium  webapp-manager  xnviewmp yazi 
+yay -S archlinux-xdg-menu pacsea 7zip blender brave-bin dolphin freerdp gnome-calculator greetd evtest heroic-games-launcher-bin hwinfo kitty lazygit onlyoffice-bin krita neovim networkmanager-openvpn networkmanager-vpn-plugin-openconnect networkmanager-vpn-plugin-openvpn obs-studio okular signal-desktop steam spotify-launcher stow sunshine teams-for-linux vlc vscodium  webapp-manager  xnviewmp yazi 
 #yay -S xf86-input-libinput xf86-input-wacom wacom-utility-git wacomtablet input-wacom-dkms-git
 #yay -S winboat 

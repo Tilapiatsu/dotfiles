82f3a4c3d9c989ba092f73198e25da554d8400dc
Tilapiatsu <tilapiatsu@hotmail.fr>
2026-01-04 01:39:53 +0100

    * Update config
    + Add rclone config
    + Update Various config

---
+++ rclone_mnt.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+home='/home/tilapiatsu'
+cp ${home}/TILAPIATSU/RCLONE/GDRIVE/RESSOURCES/mnt-ressources.mount /etc/systemd/system/mnt-ressources.mount
+cp ${home}/TILAPIATSU/RCLONE/GDRIVE/RESSOURCES/mnt-ressources.automount /etc/systemd/system/mnt-ressources.automount
+cp ${home}/TILAPIATSU/RCLONE/GDRIVE/SOFTWARE_DATA/mnt-software_data.mount /etc/systemd/system/mnt-software_data.mount
+cp ${home}/TILAPIATSU/RCLONE/GDRIVE/SOFTWARE_DATA/mnt-software_data.automount /etc/systemd/system/mnt-software_data.automount
+
+systemctl daemon-reload
+systemctl enable --now mnt-ressources.automount
+systemctl enable --now mnt-software_data.automount

793e0eeb6c3ad5d0ce0c4cf5fe0b4db9f54e66a6
Tilapiatsu <tilapiatsu@hotmail.fr>
2026-01-11 17:10:44 +0100

    + Add Lazyvim Config

---
+++ lazyvim/.config/nvim/lua/plugins/material_theme.lua
@@ -0,0 +1,22 @@
+return {
+  "marko-cerovac/material.nvim",
+  priority = 1000, -- Make sure to load this before all the other start plugins.
+  config = function()
+    require("material").setup({
+      styles = {
+        comments = {
+          italic = true,
+        }, -- Disable italics in comments
+      },
+
+      custom_colors = function(colors)
+        --colors.editor.bg = "#191a1e"
+        --colors.main.purple = "#191a1e"
+        colors.editor.cursor = "#225566"
+        --colors.lsp.error = '#852029'
+        --colors.editor.selection = '#222244'
+      end,
+    })
+    vim.cmd.colorscheme("material-deep-ocean")
+  end,
+}
---
+++ lazyvim/.config/nvim/lua/plugins/twilight.lua
@@ -0,0 +1,15 @@
+if true then
+  return {}
+end
+
+return {
+  -- Lua
+  {
+    "folke/twilight.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+}

d2baf90973586255f5a8c702cf36740879f567c0
Tilapiatsu <tilapiatsu@hotmail.fr>
2026-01-12 01:40:22 +0100

    * update lazyvim config
    + add python lsp and debuger
    * update rust debuger

---
+++ lazyvim/.config/nvim/lua/plugins/python.lua
@@ -0,0 +1,85 @@
+-- if true then
+--   return {}
+-- end
+
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "ninja", "rst" } },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ruff = {
+          cmd_env = { RUFF_TRACE = "messages" },
+          init_options = {
+            settings = {
+              logLevel = "error",
+            },
+          },
+          keys = {
+            {
+              "<leader>co",
+              LazyVim.lsp.action["source.organizeImports"],
+              desc = "Organize Imports",
+            },
+          },
+        },
+        ruff_lsp = {
+          keys = {
+            {
+              "<leader>co",
+              LazyVim.lsp.action["source.organizeImports"],
+              desc = "Organize Imports",
+            },
+          },
+        },
+      },
+      --   setup = {
+      --     [ruff] = function()
+      --       Snacks.util.lsp.on({ name = ruff }, function(_, client)
+      --         -- Disable hover in favor of Pyright
+      --         client.server_capabilities.hoverProvider = false
+      --       end)
+      --     end,
+      --   },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      local servers = { "pyright", "basedpyright", "ruff", "ruff_lsp", ruff, lsp }
+      for _, server in ipairs(servers) do
+        opts.servers[server] = opts.servers[server] or {}
+        opts.servers[server].enabled = server == lsp or server == ruff
+      end
+    end,
+  },
+  {
+    "nvim-neotest/neotest-python",
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+  -- stylua: ignore
+  keys = {
+    { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method", ft = "python" },
+    { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class", ft = "python" },
+  },
+    config = function()
+      require("dap-python").setup("debugpy-adapter")
+    end,
+  },
+  {
+    "linux-cultist/venv-selector.nvim",
+    cmd = "VenvSelect",
+    opts = {
+      options = {
+        notify_user_on_venv_activation = true,
+      },
+    },
+    --  Call config for Python files and load the cached venv automatically
+    ft = "python",
+    keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv", ft = "python" } },
+  },
+}

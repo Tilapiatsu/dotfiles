3981769e5b004d12acb21a0148647ff12a9b14c4
Tilapiatsu <tilapiatsu@hotmail.fr>
2026-01-14 00:44:48 +0100

    * Update Config

--- lazyvim/.config/nvim/lua/plugins/material_theme.lua
+++ lazyvim/.config/nvim/lua/plugins/material_theme.lua
@@ -1,3 +1,7 @@
+-- if true then
+--   return {}
+-- end
+--
 return {
   "marko-cerovac/material.nvim",
   priority = 1000, -- Make sure to load this before all the other start plugins.
@@ -11,10 +15,10 @@ return {
 
       custom_colors = function(colors)
         --colors.editor.bg = "#191a1e"
-        --colors.main.purple = "#191a1e"
-        colors.editor.cursor = "#225566"
+        colors.editor.bg = "#13161e"
+        colors.editor.cursor = "#255869"
         --colors.lsp.error = '#852029'
-        --colors.editor.selection = '#222244'
+        colors.editor.selection = "#232934"
       end,
     })
     vim.cmd.colorscheme("material-deep-ocean")

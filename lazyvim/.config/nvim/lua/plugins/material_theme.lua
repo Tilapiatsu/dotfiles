101e818539b56240bf9bfaedb4fa7049bfb6a09d
Tilapiatsu <tilapiatsu@hotmail.fr>
2026-02-01 23:40:16 +0100

    * Update Lazyvim Material Theme

--- lazyvim/.config/nvim/lua/plugins/material_theme.lua
+++ lazyvim/.config/nvim/lua/plugins/material_theme.lua
@@ -1,7 +1,7 @@
 -- if true then
 --   return {}
 -- end
---
+
 return {
   "marko-cerovac/material.nvim",
   priority = 1000, -- Make sure to load this before all the other start plugins.
@@ -14,11 +14,25 @@ return {
       },
 
       custom_colors = function(colors)
-        --colors.editor.bg = "#191a1e"
-        colors.editor.bg = "#13161e"
+        -- Dark
+        -- colors.editor.bg = "#13161e"
+        -- colors.editor.selection = "#122231"
+
+        -- Brighter
+        colors.editor.bg = "#252830"
+        colors.editor.bg_alt = "#2a2d36"
         colors.editor.cursor = "#255869"
-        --colors.lsp.error = '#852029'
-        colors.editor.selection = "#122231"
+        colors.editor.selection = "#2b3743"
+        colors.editor.active = "#1e222a"
+        colors.syntax.comments = "#4f5568"
+        colors.editor.contrast = "#1a1e25"
+        colors.backgrounds.sidebars = colors.editor.bg
+        colors.backgrounds.floating_windows = "#1d1f26"
+        colors.backgrounds.non_current_windows = colors.editor.bg
+        colors.backgrounds.bg_blend = colors.editor.bg
+        colors.backgrounds.cursor_line = colors.editor.active
+        --colors.editor.contrast = "#1b1e25"
+        --colors.lsp.error = "#273746"
       end,
     })
     vim.cmd.colorscheme("material-deep-ocean")

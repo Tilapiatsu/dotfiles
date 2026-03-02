f56d50d088d240c285453a1ea254898f121823a6
Tilapiatsu <tilapiatsu@hotmail.fr>
2026-03-03 00:45:09 +0100

    * update package list

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-#pacman -Syu
-#pacman -S yay
-yay -S ark archlinux-xdg-menu pacsea 7zip brave-bin btop darktable dolphin freerdp gimp gnome-calculator greetd evtest heroic-games-launcher-bin hwinfo kitty lazygit onlyoffice-bin krita neovim networkmanager-openvpn networkmanager-vpn-plugin-openconnect networkmanager-vpn-plugin-openvpn obs-studio obsidian okular signal-desktop snitch-bin steam spotify-launcher stow sunshine superfile teams-for-linux-bin vlc vscodium webapp-manager wine xnviewmp yazi
+# pacman -Syu
+pacman -S yay
+yay -S ark ark-vcs-bin archlinux-xdg-menu pacsea 7zip brave-bin btop darktable dolphin discord dkms freerdp gimp gnome-calculator greetd-dms-greeter-git dms-shell-bin evtest heroic-games-launcher-bin kdesvn hwinfo kitty lazygit luarocks onlyoffice-bin krita neovim networkmanager-openvpn networkmanager-vpn-plugin-openconnect networkmanager-vpn-plugin-openvpn obs-studio obsidian okular onlyoffice-bin pacsea-bin pacseek rclone signal-desktop snitch-bin steam spotify-launcher stow sunshine superfile syncthing teams-for-linux-bin ttf-opensans uv vlc vscodium webapp-manager wine xnviewmp yazi
 #yay -S xf86-input-libinput xf86-input-wacom wacom-utility-git wacomtablet input-wacom-dkms-git
 #yay -S winboat
+# yay -S --needed - <pkglist.txt

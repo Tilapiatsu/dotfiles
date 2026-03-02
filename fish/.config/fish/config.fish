698be19db3d4090da11fa604a2c48689d2535236
Tilapiatsu <tilapiatsu@hotmail.fr>
2026-03-02 19:26:33 +0100

    * Update config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,11 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 starship init fish | source
 
+alias lg="lazygit"
+alias nv="nvim"
+alias dot="~/DOTFILES/"
+alias tila="~/TILAPIATSU/"
+alias projects="~/PROJECTS/"
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting

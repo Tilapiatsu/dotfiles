7d74766c0a580aa187f69ec2be9e52ee1a41c7bf
Tilapiatsu <tilapiatsu@hotmail.fr>
2026-03-03 22:26:26 +0100

    * Update install script

--- install.sh
+++ install.sh
@@ -5,8 +5,31 @@
 # NOTE: 2.Install Yay
 pacman -S yay
 
-# NOTE: 3.Install Main packages
-yay -S ark ark-vcs-bin archlinux-xdg-menu pacsea-bin 7zip brave-bin btop darktable dolphin discord dkms freerdp gimp gnome-calculator greetd-dms-greeter-git dms-shell-bin evtest heroic-games-launcher-bin kdesvn hwinfo kitty lazygit luarocks onlyoffice-bin krita neovim networkmanager-openvpn networkmanager-vpn-plugin-openconnect mingw-w64-bin networkmanager-vpn-plugin-openvpn obs-studio obsidian okular onlyoffice-bin pacsea-bin pacseek qt6ct rclone signal-desktop snitch-bin steam spotify-launcher stow sunshine superfile syncthing teams-for-linux-bin tree-sitter-cli ttf-opensans ttf-firacode-nerd uv vlc vscodium webapp-manager wine xnviewmp yazi
+# NOTE: 3.Install stow
+yay -S stow
+
+# NOTE: 4.Stow everything
+stow Ark-Vcs
+stow blender
+stow btop
+stow DankMaterialShell
+stow dolphin
+stow fish
+stow gtk-3.0
+stow gtk-4.0
+stow kitty
+stow lazyvim
+stow mimeapps
+rm -r ~/.config/niri
+stow niri
+stow qt5ct
+stow qt6ct
+stow starship
+stow superfile
+stow xdg-open
+
+# NOTE: 4.Install Main packages
+yay -S ark ark-vcs-bin archlinux-xdg-menu 7zip brave-bin btop darktable dolphin discord dkms freerdp gimp gnome-calculator greetd-dms-greeter-git dms-shell-bin evtest heroic-games-launcher-bin kdesvn hwinfo kitty lazygit luarocks onlyoffice-bin krita neovim networkmanager-openvpn networkmanager-vpn-plugin-openconnect mingw-w64-bin networkmanager-vpn-plugin-openvpn obs-studio obsidian okular pacsea-bin pacseek qt6ct rclone signal-desktop snitch-bin steam spotify-launcher sunshine superfile syncthing teams-for-linux-bin tree-sitter-cli ttf-opensans ttf-firacode-nerd uv vlc vscodium webapp-manager wine xnviewmp yazi
 
 # NOTE: 4.Install starship : https://starship.rs/guide/
 curl -sS https://starship.rs/install.sh | sh
@@ -17,6 +40,7 @@ curl -fsSL https://install.danklinux.com | sh
 # NOTE: 6.Install dms-greeter ( Loggin manager ) : https://danklinux.com/docs/dankgreeter/installation
 dms greeter install
 
+# NOTE: 7.Stow everything
 #yay -S xf86-input-libinput xf86-input-wacom wacom-utility-git wacomtablet input-wacom-dkms-git
 #yay -S winboat
 # yay -S --needed - <pkglist.txt
